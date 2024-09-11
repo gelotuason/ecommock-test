3b3a9652d2db7b8716eaa6c5987872cb1fbf877f
gelotuason <gelotuason@gmail.com>
2024-09-11 21:05:07 +0800

    feat: mobile nav

---
+++ src/components/nav.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
+import { Button } from './ui/button';
+import {
+    Search,
+    User,
+    Heart,
+    ShoppingCart,
+    Menu,
+    ChevronRight,
+    X
+} from 'lucide-react';
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
+
+
+export default function Nav() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <nav className='flex justify-around items-center gap-1 py-2'>
+            <Drawer open={isOpen} onOpenChange={setIsOpen} direction='left'>
+                <DrawerTrigger>
+                    <Menu />
+                </DrawerTrigger>
+                <DrawerContent className='h-screen md:w-1/4 border-none bg-secondary'>
+                    <DrawerHeader className='flex justify-between items-center bg-primary text-secondary '>
+                        <DrawerTitle>Menu</DrawerTitle>
+                        <DrawerDescription className='sr-only' />
+                        <DrawerClose>
+                            <X />
+                        </DrawerClose>
+                    </DrawerHeader>
+                    <div className='flex flex-col gap-8 px-6'>
+                        <div>
+                            <Button asChild variant="ghost" className='w-full border-b rounded-none flex justify-between py-6'>
+                                <Link href='/'>
+                                    Home
+                                    <ChevronRight />
+                                </Link>
+                            </Button>
+                            <Button asChild variant="ghost" className='w-full border-b rounded-none flex justify-between py-6'>
+                                <Link href='/'>
+                                    Shop
+                                    <ChevronRight />
+                                </Link>
+                            </Button>
+                        </div>
+                        <div>
+                            <Button variant='ghost' asChild className='w-full border-b rounded-none flex justify-start gap-4 py-6'>
+                                <Link href='/'>
+                                    <Heart className="size-5" />
+                                    Wishlist
+                                </Link>
+                            </Button>
+                            <Button variant='ghost' asChild className='w-full border-b rounded-none flex justify-start gap-4 py-6'>
+                                <Link href='/'>
+                                    <User />
+                                    Login
+                                </Link>
+                            </Button>
+                        </div>
+                    </div>
+                </DrawerContent>
+            </Drawer>
+
+            <h1 className='text-xl'>ecommock.</h1>
+
+            <div className='flex gap-1'>
+                <Button variant='ghost' size='icon'>
+                    <Search />
+                </Button>
+                <Button variant='ghost' size='icon'>
+                    <ShoppingCart />
+                </Button>
+            </div>
+        </nav>
+    )
+}
