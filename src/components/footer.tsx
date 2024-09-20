2b7092d25ea4fbf0aebb94b637755ea9c8dc1052
gelotuason <gelotuason@gmail.com>
2024-09-20 22:22:01 +0800

    updated footer content

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,25 +3,49 @@ import {
     AccordionContent,
     AccordionItem,
     AccordionTrigger,
-} from "@/components/ui/accordion"
+} from "@/components/ui/accordion";
+
+import { Mail, Linkedin, Github } from "lucide-react";
+import Link from "next/link";
 
 export default function Footer() {
     // TODO: integrate api category on shop highlights
 
     return (
-        <footer className="bg-primary text-white px-6 py-2">
+        <footer className="bg-primary text-white px-6 py-4 grid">
             {/* shop highlights */}
             <Accordion type="single" collapsible>
                 <AccordionItem className="border-accent" value="item-1">
                     <AccordionTrigger className="text-lg">Shop Highlights</AccordionTrigger>
-                    <AccordionContent className="text-accent text-base space-y-2">
-                        <p>Men</p>
-                        <p>Women</p>
-                        <p>Jewelries</p>
-                        <p>Electronics</p>
+                    <AccordionContent className="text-accent text-base">
+                        <div className="flex flex-col">
+                            <Link href='/'>Men</Link>
+                            <Link href='/'>Women</Link>
+                            <Link href='/'>Jewelries</Link>
+                            <Link href='/'>Electronics</Link>
+                        </div>
                     </AccordionContent>
                 </AccordionItem>
             </Accordion>
+
+            <Accordion type="single" collapsible>
+                <AccordionItem className="border-accent" value="item-1">
+                    <AccordionTrigger className="text-lg">Quick links</AccordionTrigger>
+                    <AccordionContent className="text-accent text-base">
+                        <div className="flex flex-col">
+                            <Link href='/'>Home</Link>
+                            <Link href='/'>Shop</Link>
+                        </div>
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+
+            <p className="text-accent text-center mt-10 mb-2">Developed by <strong>Ferangelo Tuason</strong>.</p>
+            <div className="flex justify-center gap-3">
+                <Link href='/'><Mail size={20} strokeWidth={1} /></Link>
+                <Link href='/'><Linkedin size={20} strokeWidth={1} /></Link>
+                <Link href='/'><Github size={20} strokeWidth={1} /></Link>
+            </div>
         </footer>
     )
 }
