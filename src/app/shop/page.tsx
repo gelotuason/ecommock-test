2be620d9e64289dcbcbbf41a8b1e3e357a688e4b
gelotuason <gelotuason@gmail.com>
2024-09-21 18:20:04 +0800

    added filter dropdown

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -7,6 +7,8 @@ import Image from "next/image";
 import { ListFilter, ArrowDownUp, LayoutGrid, List, Star, Heart, Search, ShoppingCart } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { Slider } from "@/components/ui/slider";
+
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -15,17 +17,6 @@ import {
     BreadcrumbPage,
     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-
-import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger,
-} from "@/components/ui/drawer";
 
 import {
     DropdownMenu,
@@ -54,8 +45,6 @@ export default function Shop() {
     const [showZtoA, setShowZtoA] = useState<Checked>(false);
     const [showPriceLowToHigh, setShowPriceLowToHigh] = useState<Checked>(false);
     const [showPriceHighToLow, setShowPriceHighToLow] = useState<Checked>(false);
-
-    // TODO: filter drawer ui
 
     return (
         <main>
@@ -166,32 +155,32 @@ export default function Shop() {
                 {/* control bar */}
                 <div className="flex justify-between items-center">
                     <div className="flex gap-1 items-center">
-                        <Drawer direction="left">
-                            <DrawerTrigger className="flex items-center px-4 space-x-2 bg-primary text-white rounded py-1">
+                        <DropdownMenu>
+                            <DropdownMenuTrigger className="flex items-center px-4 space-x-2 bg-primary text-white rounded py-1">
                                 <ListFilter size={12} />
                                 <small>FILTER</small>
-                            </DrawerTrigger>
-                            <DrawerContent className="h-screen">
-                                <DrawerHeader>
-                                    <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-                                    <DrawerDescription>This action cannot be undone.</DrawerDescription>
-                                </DrawerHeader>
-                                <DrawerFooter>
-                                    <Button>Submit</Button>
-                                    <DrawerClose>
-                                        <Button variant="outline">Cancel</Button>
-                                    </DrawerClose>
-                                </DrawerFooter>
-                            </DrawerContent>
-                        </Drawer>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="min-w-[12rem] pb-2">
+                                <DropdownMenuLabel>Filter by:</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem className="flex flex-col items-start gap-2">
+                                    <p className="text-base">Price: $0.00 - $399.99</p>
+                                    <Slider defaultValue={[0]} max={100} step={0.1} />
+                                </DropdownMenuItem>
+                                <DropdownMenuItem className="flex flex-col items-start gap-2">
+                                    <p className="text-base">Rating: 0</p>
+                                    <Slider defaultValue={[0]} max={5} step={1} />
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
 
                         <DropdownMenu>
                             <DropdownMenuTrigger className="flex items-center px-4 space-x-2 border text-primary rounded py-1">
                                 <ArrowDownUp size={12} />
-                                <small>SORT BY</small>
+                                <small>SORT</small>
                             </DropdownMenuTrigger>
                             <DropdownMenuContent className="w-56">
-                                <DropdownMenuLabel>Sort by</DropdownMenuLabel>
+                                <DropdownMenuLabel>Sort by:</DropdownMenuLabel>
                                 <DropdownMenuSeparator />
                                 <DropdownMenuCheckboxItem
                                     checked={showBestselling}
