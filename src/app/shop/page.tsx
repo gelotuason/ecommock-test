43a84fcb0bff9c29d79479330c3f25e556d19137
gelotuason <gelotuason@gmail.com>
2024-09-21 17:00:54 +0800

    added product list

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,8 +1,12 @@
 'use client';
 
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+import { ListFilter, ArrowDownUp, LayoutGrid, List, Star, Heart, Search, ShoppingCart } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -12,6 +16,29 @@ import {
     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+    DropdownMenuCheckboxItem,
+} from "@/components/ui/dropdown-menu";
+
+import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
+
 import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
@@ -19,7 +46,17 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import '../css/category-swiper.css';
 
+type Checked = DropdownMenuCheckboxItemProps["checked"]
+
 export default function Shop() {
+    const [showBestselling, setShowBestselling] = useState<Checked>(false);
+    const [showAtoZ, setShowAtoZ] = useState<Checked>(false);
+    const [showZtoA, setShowZtoA] = useState<Checked>(false);
+    const [showPriceLowToHigh, setShowPriceLowToHigh] = useState<Checked>(false);
+    const [showPriceHighToLow, setShowPriceHighToLow] = useState<Checked>(false);
+
+    // TODO: filter drawer ui
+
     return (
         <main>
             {/* breadcrumb */}
@@ -123,6 +160,279 @@ export default function Shop() {
                 {/* end of product listing */}
             </section>
             {/* end of categories */}
-        </main>
+
+            {/* product list */}
+            <section className="px-4 py-10">
+                {/* control bar */}
+                <div className="flex justify-between items-center">
+                    <div className="flex gap-1 items-center">
+                        <Drawer direction="left">
+                            <DrawerTrigger className="flex items-center px-4 space-x-2 bg-primary text-white rounded py-1">
+                                <ListFilter size={12} />
+                                <small>FILTER</small>
+                            </DrawerTrigger>
+                            <DrawerContent className="h-screen">
+                                <DrawerHeader>
+                                    <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                                    <DrawerDescription>This action cannot be undone.</DrawerDescription>
+                                </DrawerHeader>
+                                <DrawerFooter>
+                                    <Button>Submit</Button>
+                                    <DrawerClose>
+                                        <Button variant="outline">Cancel</Button>
+                                    </DrawerClose>
+                                </DrawerFooter>
+                            </DrawerContent>
+                        </Drawer>
+
+                        <DropdownMenu>
+                            <DropdownMenuTrigger className="flex items-center px-4 space-x-2 border text-primary rounded py-1">
+                                <ArrowDownUp size={12} />
+                                <small>SORT BY</small>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-56">
+                                <DropdownMenuLabel>Sort by</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuCheckboxItem
+                                    checked={showBestselling}
+                                    onCheckedChange={setShowBestselling}
+                                >
+                                    Best selling
+                                </DropdownMenuCheckboxItem>
+                                <DropdownMenuCheckboxItem
+                                    checked={showAtoZ}
+                                    onCheckedChange={setShowAtoZ}
+                                >
+                                    Alphabetically, A-Z
+                                </DropdownMenuCheckboxItem>
+                                <DropdownMenuCheckboxItem
+                                    checked={showZtoA}
+                                    onCheckedChange={setShowZtoA}
+                                >
+                                    Alphabetically, Z-A
+                                </DropdownMenuCheckboxItem>
+                                <DropdownMenuCheckboxItem
+                                    checked={showPriceLowToHigh}
+                                    onCheckedChange={setShowPriceLowToHigh}
+                                >
+                                    Price, low to high
+                                </DropdownMenuCheckboxItem>
+                                <DropdownMenuCheckboxItem
+                                    checked={showPriceHighToLow}
+                                    onCheckedChange={setShowPriceHighToLow}
+                                >
+                                    Price, high to low
+                                </DropdownMenuCheckboxItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+
+                    <div className="flex">
+                        <Button variant='ghost' size='icon'>
+                            <LayoutGrid strokeWidth={1} size={20} />
+                        </Button>
+                        <Button variant='ghost' size='icon'>
+                            <List strokeWidth={1} size={20} />
+                        </Button>
+                    </div>
+                </div>
+                {/* end of control bar */}
+
+                {/* products */}
+                <div className="grid grid-cols-2 gap-x-4 gap-y-4 text-center">
+                    {/* product 1 */}
+                    <div>
+                        {/* product image */}
+                        <div className="relative h-[120px] mb-2 py-2">
+                            <Link
+                                href='/'
+                            >
+                                <Image
+                                    src='/slides-img-3.jpg'
+                                    fill
+                                    alt="Best Seller 1"
+                                    className="object-contain"
+                                />
+
+                                <div className="relative w-max mx-auto flex items-end h-full">
+                                    <div className="bg-[#f5f5f5] rounded flex items-center divide-x">
+                                        <button className="p-1 hover:bg-black hover:text-white transition-all duration-300">
+                                            <ShoppingCart size={20} strokeWidth={1} />
+                                        </button>
+                                        <button className="p-1 hover:bg-black hover:text-white transition-all duration-300">
+                                            <Heart size={20} strokeWidth={1} />
+                                        </button>
+                                        <button className="p-1 hover:bg-black hover:text-white transition-all duration-300">
+                                            <Search size={20} strokeWidth={1} />
+                                        </button>
+                                    </div>
+                                </div>
+
+                            </Link>
+                            {/* <button className="hidden md:block text-sm font-bold bg-white w-max px-4 py-1 mt-4">SHOP MORE</button> */}
+                        </div>
+                        {/* end of product image */}
+
+                        {/* product details */}
+                        <div>
+                            <p className="text-lg">title</p>
+                            <div className="flex justify-center">
+                                {Array.from({ length: 5 }).map(star => (
+                                    <Star fill="black" size={16} />
+                                ))}
+                            </div>
+                            <p className="text-lg">$36.00</p>
+                        </div>
+                        {/* end of product details */}
+                    </div>
+                    {/* end of product 1 */}
+
+                    {/* product 2 */}
+                    <div>
+                        {/* product image */}
+                        <div className="relative h-[120px] mb-2 py-2">
+                            <Link
+                                href='/'
+                                className=""
+                            >
+                                <Image
+                                    src='/slides-img-3.jpg'
+                                    fill
+                                    alt="Best Seller 1"
+                                    className="object-contain"
+                                />
+
+                                <div className="relative w-max mx-auto flex items-end h-full">
+                                    <div className="bg-[#f5f5f5] rounded flex items-center divide-x">
+                                        <button className="p-1 hover:bg-black hover:text-white transition-all duration-300">
+                                            <ShoppingCart size={20} strokeWidth={1} />
+                                        </button>
+                                        <button className="p-1 hover:bg-black hover:text-white transition-all duration-300">
+                                            <Heart size={20} strokeWidth={1} />
+                                        </button>
+                                        <button className="p-1 hover:bg-black hover:text-white transition-all duration-300">
+                                            <Search size={20} strokeWidth={1} />
+                                        </button>
+                                    </div>
+                                </div>
+
+                            </Link>
+                            {/* <button className="hidden md:block text-sm font-bold bg-white w-max px-4 py-1 mt-4">SHOP MORE</button> */}
+                        </div>
+                        {/* end of product image */}
+
+                        {/* product details */}
+                        <div>
+                            <p className="text-lg">title</p>
+                            <div className="flex justify-center">
+                                {Array.from({ length: 5 }).map(star => (
+                                    <Star fill="black" size={16} />
+                                ))}
+                            </div>
+                            <p className="text-lg">$36.00</p>
+                        </div>
+                        {/* end of product details */}
+                    </div>
+                    {/* end of product 2 */}
+
+                    {/* product 3 */}
+                    <div>
+                        {/* product image */}
+                        <div className="relative h-[120px] mb-2 py-2">
+                            <Link
+                                href='/'
+                                className=""
+                            >
+                                <Image
+                                    src='/slides-img-3.jpg'
+                                    fill
+                                    alt="Best Seller 1"
+                                    className="object-contain"
+                                />
+
+                                <div className="relative w-max mx-auto flex items-end h-full">
+                                    <div className="bg-[#f5f5f5] rounded flex items-center divide-x">
+                                        <button className="p-1 hover:bg-black hover:text-white transition-all duration-300">
+                                            <ShoppingCart size={20} strokeWidth={1} />
+                                        </button>
+                                        <button className="p-1 hover:bg-black hover:text-white transition-all duration-300">
+                                            <Heart size={20} strokeWidth={1} />
+                                        </button>
+                                        <button className="p-1 hover:bg-black hover:text-white transition-all duration-300">
+                                            <Search size={20} strokeWidth={1} />
+                                        </button>
+                                    </div>
+                                </div>
+
+                            </Link>
+                            {/* <button className="hidden md:block text-sm font-bold bg-white w-max px-4 py-1 mt-4">SHOP MORE</button> */}
+                        </div>
+                        {/* end of product image */}
+
+                        {/* product details */}
+                        <div>
+                            <p className="text-lg">title</p>
+                            <div className="flex justify-center">
+                                {Array.from({ length: 5 }).map(star => (
+                                    <Star fill="black" size={16} />
+                                ))}
+                            </div>
+                            <p className="text-lg">$36.00</p>
+                        </div>
+                        {/* end of product details */}
+                    </div>
+                    {/* end of product 3 */}
+
+                    {/* product 4 */}
+                    <div>
+                        {/* product image */}
+                        <div className="relative h-[120px] mb-2 py-2">
+                            <Link
+                                href='/'
+                            >
+                                <Image
+                                    src='/slides-img-3.jpg'
+                                    fill
+                                    alt="Best Seller 1"
+                                    className="object-contain"
+                                />
+
+                                <div className="relative w-max mx-auto flex items-end h-full">
+                                    <div className="bg-[#f5f5f5] rounded flex items-center divide-x">
+                                        <button className="p-1 hover:bg-black hover:text-white transition-all duration-300">
+                                            <ShoppingCart size={20} strokeWidth={1} />
+                                        </button>
+                                        <button className="p-1 hover:bg-black hover:text-white transition-all duration-300">
+                                            <Heart size={20} strokeWidth={1} />
+                                        </button>
+                                        <button className="p-1 hover:bg-black hover:text-white transition-all duration-300">
+                                            <Search size={20} strokeWidth={1} />
+                                        </button>
+                                    </div>
+                                </div>
+
+                            </Link>
+                            {/* <button className="hidden md:block text-sm font-bold bg-white w-max px-4 py-1 mt-4">SHOP MORE</button> */}
+                        </div>
+                        {/* end of product image */}
+
+                        {/* product details */}
+                        <div>
+                            <p className="text-lg">title</p>
+                            <div className="flex justify-center">
+                                {Array.from({ length: 5 }).map(star => (
+                                    <Star fill="black" size={16} />
+                                ))}
+                            </div>
+                            <p className="text-lg">$36.00</p>
+                        </div>
+                        {/* end of product details */}
+                    </div>
+                    {/* end of product 4 */}
+                </div>
+                {/* end of products */}
+            </section>
+            {/* end of product list */}
+        </main >
     )
 }
