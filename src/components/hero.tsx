9f2922f99d165e0ba563f4c7f610de64de7bed18
gelotuason <gelotuason@gmail.com>
2024-09-22 22:38:40 +0800

    add conditional rendering

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -23,35 +23,26 @@ type SlidesProps = {
     imgSrc: string
     text: string
     credit: string
-    slug: string
 }
 
 const slides: SlidesProps[] = [
-    { imgSrc: Slide1.src, text: 'Slide 1', credit: 'Kelly Sikkema', slug: '' },
-    { imgSrc: Slide2.src, text: 'Slide 2', credit: 'Jacob Vega', slug: '' },
-    { imgSrc: Slide3.src, text: 'Slide 3', credit: 'Joel Muniz', slug: '' },
-    { imgSrc: Slide4.src, text: 'Slide 3', credit: 'Samuel Dixon', slug: '' },
+    { imgSrc: Slide1.src, text: 'Slide 1', credit: 'Kelly Sikkema' },
+    { imgSrc: Slide2.src, text: 'Slide 2', credit: 'Jacob Vega' },
+    { imgSrc: Slide3.src, text: 'Slide 3', credit: 'Joel Muniz' },
+    { imgSrc: Slide4.src, text: 'Slide 3', credit: 'Samuel Dixon' },
 ]
 
 export default function Hero() {
+    // TODO: pagination bullet visibility
 
-    const { categories, status, error } = useAppSelector(state => state.categoryNamesReducer);
+    const { categories, status } = useAppSelector(state => state.categoryNamesReducer);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
-        if (status === 'idle') {
-            dispatch(fetchCategoryNames());
-        }
-    }, []);
+        dispatch(fetchCategoryNames());
+    }, [dispatch]);
 
-    if (status === 'succeeded') {
-        slides.map((slide, index) => {
-            slide.slug = categories[index];
-        });
-    }
-
-    // TODO: pagination bullet visibility
-    // TODO: error handling
+    const isLoading = status === 'loading' || status === 'idle' || status === 'failed';
 
     return (
         <section>
@@ -68,28 +59,36 @@ export default function Hero() {
                 }}
                 style={{ height: '500px' }}
             >
-                {slides.map((slide, index) => (
-                    <SwiperSlide
-                        key={index}
-                        style={{
-                            backgroundImage: `url('${slide.imgSrc}')`,
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'top 0 left -100px',
-                            backgroundRepeat: 'no-repeat',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            alignItems: 'start',
-                            paddingLeft: '2rem',
-                            paddingRight: '2rem',
-                            gap: '2rem',
-                        }}
-                    >
-                        <h1 className='text-5xl font-bold text-white/60'>Lorem ipsum dolor sit amet.</h1>
-                        <Link href={`/shop/${slide.slug}`} className='text-white/80 text-xl border-2 border-white/80 px-6 py-2 hover:bg-black hover:border-black transition-all duration-300'>Shop now</Link>
-                        <small className='text-white/60 font-light bottom-1 right-1 absolute'>Photo by {slide.credit} on Unsplash.</small>
-                    </SwiperSlide>
-                ))}
+                {slides.map((slide, index) => {
+                    const slug = isLoading ? '' : categories[index];
+
+                    return (
+                        <SwiperSlide
+                            key={index}
+                            style={{
+                                backgroundImage: `url('${slide.imgSrc}')`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'top 0 left -100px',
+                                backgroundRepeat: 'no-repeat',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'center',
+                                alignItems: 'start',
+                                paddingLeft: '2rem',
+                                paddingRight: '2rem',
+                                gap: '2rem',
+                            }}
+                        >
+                            <h1 className='text-5xl font-bold text-white/60'>Lorem ipsum dolor sit amet.</h1>
+                            {!isLoading && (
+                                <Link href={`/shop/${slug}`} className='text-white/80 text-xl border-2 border-white/80 px-6 py-2 hover:bg-black hover:border-black transition-all duration-300'>
+                                    Shop now
+                                </Link>
+                            )}
+                            <small className='text-white/60 font-light bottom-1 right-1 absolute'>Photo by {slide.credit} on Unsplash.</small>
+                        </SwiperSlide>
+                    )
+                })}
             </Swiper>
         </section>
     )
